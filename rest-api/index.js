e269b3e492efcec02dbc4407821390954e36cc96
Mohamed-aden <aden6864@bears.unco.edu>
2022-02-25 19:19:24 -0700

    With the database connected

--- rest-api/index.js
+++ rest-api/index.js
@@ -2,6 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js";
 
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://moaden:0o31Zzjio89IRH0b@cluster0.uld1m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("Database not Connected" + err);
+  });
+
 const app = express();
 
 const PORT = 5000;
